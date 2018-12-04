2812c3158fb3727aec358ffa1d2f763472a20fbf
mayank0809 <hellomayanksahay@gmail.com>
2018-12-04 15:51:16 +0530

    Update EmployeeModel.cs

--- PFServiceClient/EmployeeModel.cs
+++ PFServiceClient/EmployeeModel.cs
@@ -1,18 +1,18 @@
-using System.Data.Entity;
+//using System.Data.Entity;
 
 namespace PFServiceClient
 {
-    public class EmployeeModel : DbContext
+    public class EmployeeModel //: DbContext
     {
         public EmployeeModel()
             : base("name=EmployeeModel")
         {
         }
 
-        public virtual DbSet<Employee> Employees { get; set; }
+        //public virtual DbSet<Employee> Employees { get; set; }
 
-        protected override void OnModelCreating(DbModelBuilder modelBuilder)
-        {
-        }
+        //protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        //{
+        //}
     }
 }
